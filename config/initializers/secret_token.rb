b74735db21865b3efbabafa04008d83fa2192574
Tulsee Doshi <tulsee27@gmail.com>
2014-06-16 01:33:37 +0100

    static page templates done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ICAWebsite::Application.config.secret_token = 'ada9c0aa80d128db85ce4748a45d88b6f93c29853a97ac6b9057d16b8340abbdb33915d1eb4deef586f4e6f8c1f5b47f9b92f05fbb9ba7cb58ef6fdcb52da966'
